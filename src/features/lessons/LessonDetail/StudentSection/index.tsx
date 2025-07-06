ee15f1fb3c70b7c7eca168e66b430cb719dc5354
James <j606888@gmail.com>
2025-07-06 21:41:34 +0800

    Update badge style

--- src/features/lessons/LessonDetail/StudentSection/index.tsx
+++ src/features/lessons/LessonDetail/StudentSection/index.tsx
@@ -1,5 +1,7 @@
 import { LessonStudent } from "@/store/slices/lessons";
 import Image from "next/image";
+import { format } from "date-fns";
+import { Frown, Smile } from "lucide-react";
 
 const StudentSection = ({ students }: { students: LessonStudent[] }) => {
   return (
@@ -8,7 +10,7 @@ const StudentSection = ({ students }: { students: LessonStudent[] }) => {
         {students.map((student) => (
           <div
             key={student.id}
-            className="flex items-center justify-between gap-2 py-3 border-b border-gray-200"
+            className="flex flex-col items-start gap-2 py-3 border-b border-gray-200"
           >
             <div className="flex items-center gap-2">
               <Image
@@ -20,9 +22,9 @@ const StudentSection = ({ students }: { students: LessonStudent[] }) => {
               />
               <p className="text-lg font-semibold">{student.name}</p>
             </div>
-            <div className="flex items-center gap-1">
-              {student.attendances.map((attendance, index) => (
-                <Round key={index} status={attendance} />
+            <div className="flex items-center gap-2 flex-wrap">
+              {student.attendances.map((attendance) => (
+                <Round key={attendance.startTime} attendance={attendance} />
               ))}
             </div>
           </div>
@@ -35,21 +37,35 @@ const StudentSection = ({ students }: { students: LessonStudent[] }) => {
   );
 };
 
+const BADGE_STYLES = {
+  attended: "bg-primary-500 text-white",
+  absent: "bg-gray-400 text-white",
+  not_started: "border border-gray-200 text-[#777777]",
+};
 const Round = ({
-  status = "not_started",
+  attendance,
 }: {
-  status?: "not_started" | "attended" | "absent";
+  attendance: {
+    startTime: string;
+    attendanceStatus: "not_started" | "attended" | "absent";
+  };
 }) => {
+  const date = new Date(attendance.startTime);
+
   return (
     <div
-      className={`w-4 h-4 rounded-full border flex items-center justify-center ${
-        status === "not_started"
-          ? "bg-gray-100 border-gray-200"
-          : status === "attended"
-          ? "bg-primary-500 border-primary-500"
-          : "bg-white border-gray-200"
+      className={`flex gap-1 items-center rounded-full text-xs font-semibold px-3 py-2 ${
+        BADGE_STYLES[attendance.attendanceStatus]
       }`}
-    ></div>
+    >
+      {attendance.attendanceStatus === "attended" && (
+        <Smile className="w-4 h-4" />
+      )}
+      {attendance.attendanceStatus === "absent" && (
+        <Frown className="w-4 h-4" />
+      )}
+      {format(date, "M/dd")}
+    </div>
   );
 };
 
