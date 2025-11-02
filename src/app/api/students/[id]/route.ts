6a7a41e7fb814526dcb0951c9201c8577f33db40
James <j606888@gmail.com>
2025-11-02 12:12:44 +0800

    Update order

--- src/app/api/students/[id]/route.ts
+++ src/app/api/students/[id]/route.ts
@@ -43,7 +43,7 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
           card: true,
         },
         orderBy: {
-          createdAt: "asc",
+          createdAt: "desc",
         },
       },
       attendanceRecords: {
